125fa847739fcd235fb8ccdc8ede25bdccf34e9d
gaspar_ilom <valentin.franck@campus.tu-berlin.de>
2018-01-06 13:43:40 +0100

    certificate storing enabled

--- Server/keyStore.py
+++ Server/keyStore.py
@@ -5,33 +5,42 @@ from passlib.hash import pbkdf2_sha512
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
-
-backend = default_backend()
-
-#only create cert if not stored in file/database!
-
-# Generate our key
-key = rsa.generate_private_key(
-    public_exponent=65537,
-    key_size=4096,
-    backend=backend
-    )
-# Write our key to disk for safe keeping
-with open("privateKey.pem", "wb") as f:
-    f.write(key.private_bytes(
-    encoding=serialization.Encoding.PEM,
-    format=serialization.PrivateFormat.TraditionalOpenSSL,
-    encryption_algorithm=serialization.BestAvailableEncryption(PRIVATE_KEY_PASS),
-    ))
-
+from cryptography import x509
+from cryptography.hazmat.primitives import hashes
+from cryptography.x509.oid import NameOID
+import datetime
 
 PRIVATE_KEY_PASS = bytes ("SecurePassphrase", 'utf-8')
 DB_NAME = 'flaskserver'
 DB_USER = 'flask-server'
 DB_PASSWORD = 'test123'
 DB_HOST = 'localhost'
-CERT_COUNTER = 0
 TABLES = {}
+
+def loadPrivateKey():
+    #only create private key if not stored in file on disk!
+    try:
+        f = open("privateKey.pem", "rb")
+        data = f.read()
+        f.close()
+        return serialization.load_pem_private_key(data, PRIVATE_KEY_PASS, default_backend())
+    except FileNotFoundError:
+        # Generate our key on first use
+        key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=4096,
+            backend=default_backend()
+            )
+        # Write our key to disk for safe keeping -> should be stored securely in production
+        with open("privateKey.pem", "wb") as f:
+            f.write(key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.TraditionalOpenSSL,
+            encryption_algorithm=serialization.BestAvailableEncryption(PRIVATE_KEY_PASS),
+            ))
+        return key
+
+privateKey = loadPrivateKey()
 
 TABLES['user'] = (
     "CREATE TABLE IF NOT EXISTS `user` ("
@@ -43,7 +52,7 @@ TABLES['user'] = (
 
 TABLES['certificates'] = (
     "CREATE TABLE IF NOT EXISTS `certificates` ("
-    "  `cert_id` varchar(30) NOT NULL,"
+    "  `cert_id` varchar(64) NOT NULL,"
     "  `username` varchar(20) NOT NULL,"
     "  `cert_data` blob NOT NULL,"
     "  PRIMARY KEY (`cert_id`),"
@@ -61,6 +70,8 @@ TABLES['revocations'] = (
     "  CONSTRAINT `rev_cert_id` FOREIGN KEY (`cert_id`) "
     "     REFERENCES `certificates` (`cert_id`) ON DELETE CASCADE"
     ") ENGINE=InnoDB")
+
+
 
 def createTables():
     cnx = mysql.connector.connect(user=DB_USER, password=DB_PASSWORD, host=DB_HOST, database=DB_NAME)
@@ -90,16 +101,49 @@ def retrieveCert():
 def revokeCert(u_id, cert_serial):
     return False
 
+#create cert from csr and return it in pem-encoding and return its serial number
+def createCert(csr):
+    issuer = x509.Name([
+        x509.NameAttribute(NameOID.COUNTRY_NAME, u"DE"),
+        x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, u"Berlin"),
+        x509.NameAttribute(NameOID.LOCALITY_NAME, u"Berlin"),
+        x509.NameAttribute(NameOID.ORGANIZATION_NAME, u"crypt-cloud"),
+        x509.NameAttribute(NameOID.COMMON_NAME, u"crypt-cloud"),
+        ])
+    cert = x509.CertificateBuilder().subject_name(
+        csr.subject
+    ).issuer_name(
+        issuer
+    ).public_key(
+        csr.public_key()
+    ).serial_number(
+        x509.random_serial_number()
+    ).not_valid_before(
+        datetime.datetime.utcnow()
+    ).not_valid_after(
+    # Our certificate will be valid for 2 years
+        datetime.datetime.utcnow() + datetime.timedelta(days=(2*365))
+    ).sign(privateKey, hashes.SHA512(), default_backend())
+
+    return cert.public_bytes(serialization.Encoding.PEM), cert.serial_number
+
+# store signed cert in DB and return it in pem-encoding,
+# return None if invalid csr (i.e. hash or username does not match)
+# input is required to be a username-string and csr in pem-encoding
 def updateCert(username, csr):
+    csr = x509.load_pem_x509_csr(csr, default_backend())
+    if not csr.is_signature_valid or not username == csr.subject.get_attributes_for_oid(NameOID.USER_ID)[0].value:
+        return None
+    cert, cert_id = createCert(csr)
+
     stmt = ("INSERT INTO certificates (cert_id, username, cert_data) VALUES (%s, %s, %s)")
     cnx = mysql.connector.connect(user=DB_USER, password=DB_PASSWORD, host=DB_HOST, database=DB_NAME)
     cur = cnx.cursor()
-    cert = ""
     try:
         cur.execute(stmt, (cert_id, username, cert))
     except mysql.connector.errors.IntegrityError as e:
+        print(e)
         return None
-    CERT_COUNTER += 1
     cnx.commit()
     cnx.close()
     return cert
@@ -107,3 +151,7 @@ def updateCert(username, csr):
 if __name__ == '__main__':
     createTables()
     createUser('admin', 'test@mail.com', 'password')
+    data = None
+    with open("csr.pem", "rb") as f:
+        data = f.read()
+    updateCert("admin", data)
