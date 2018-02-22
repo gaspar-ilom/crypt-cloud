cbe9eae0e8179d8d62c3b598873d70cfe37349ef
gaspar_ilom <valentin.franck@campus.tu-berlin.de>
2018-02-22 14:19:06 +0100

    create CA.pem when creating privateKey

--- Server/pki.py
+++ Server/pki.py
@@ -3,6 +3,8 @@ from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.backends import default_backend
 from cryptography.x509.oid import NameOID
 from cryptography import x509
+from cryptography.hazmat.primitives import hashes
+import datetime
 
 PRIVATE_KEY_PASS = bytes ("SecurePassphrase", 'utf-8')
 issuer = x509.Name([
@@ -34,6 +36,28 @@ def loadPrivateKey():
             format=serialization.PrivateFormat.TraditionalOpenSSL,
             encryption_algorithm=serialization.BestAvailableEncryption(PRIVATE_KEY_PASS),
             ))
+        #Create CA.pem certificate t be copied to clients!
+        cert = x509.CertificateBuilder().subject_name(
+            issuer
+        ).issuer_name(
+            issuer
+        ).public_key(
+            key.public_key()
+        ).serial_number(
+            x509.random_serial_number()
+        ).not_valid_before(
+            datetime.datetime.utcnow()
+        ).not_valid_after(
+        # Our certificate will be valid for 10 years
+            datetime.datetime.utcnow() + datetime.timedelta(days=3650)
+        ).add_extension(
+            x509.SubjectAlternativeName([x509.DNSName(u"localhost")]),
+            critical=False,
+        # Sign our certificate with our private key
+        ).sign(key, hashes.SHA256(), default_backend())
+        # Write our certificate out to disk.
+        with open("CA.pem", "wb") as f:
+            f.write(cert.public_bytes(serialization.Encoding.PEM))
         return key
 
 privateKey = loadPrivateKey()
