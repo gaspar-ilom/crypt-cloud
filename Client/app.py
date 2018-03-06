a8e11969742b8f58e1545f743f961f8eec1e3016
gaspar_ilom <valentin.franck@campus.tu-berlin.de>
2018-03-06 20:30:21 +0100

    completed main menu

--- Client/app.py
+++ Client/app.py
@@ -19,13 +19,16 @@ if __name__ == '__main__':
     # f.share('admin')
     # # f.share('tester')
     # retrieve_file_list()
+
+        # Make sure the notifications thread is only started now. This is necessary because easygui is not thread safe. So we need to mkae sure things are imported in the correct order! Should not be necessary with a proper gui or terminal based application!
         if not notify_activated:
             notify_activated = True
             from notification_handler import Notifications
             handler = Notifications(USER.username)
             handler.start()
 
-    handler.stop()
+    if notify_activated:
+        handler.stop()
     if USER.logout():
         print("Logged out.")
     else:
